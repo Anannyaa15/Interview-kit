21fd504f00d2763f5c4a8fc370488aa924825f1f
Anannyaa15 <anannyabhati15@gmail.com>
2026-09-10 17:08:33 +0530

    fix render port binding

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,3 @@
-import {app} from './app.js';import {connectDb} from './config/db.js';import {env} from './config/env.js';connectDb().then(()=>app.listen(env.port,()=>console.log(`API listening on http://localhost:${env.port}`))).catch(err=>{console.error(err);process.exit(1)});
+import {app} from './app.js';
+import {connectDb} from './config/db.js';
+import {env} from './config/env.js';connectDb().then(()=>app.listen(env.port,'0.0.0.0',()=>console.log(`API listening on http://0.0.0.0:${env.port}`))).catch(err=>{console.error(err);process.exit(1)});
